110ab51bab05043ee918ba4807e613324db7386f
John Adriel Benolirao <adriel.beno@gmail.com>
2018-03-07 00:11:33 +0800

    Todolist complete with data source and delegate methods

--- Todoey/ViewController.swift
+++ Todoey/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Kill Giant", "Go to High Monks", "What the hell is a High Monk???"]
+    var isChecked:[String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,41 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell")!
+        cell.textLabel?.text = itemArray[indexPath.row]
+        if isChecked.contains(String(indexPath.row)){
+            cell.accessoryType = .checkmark
+        } else{
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        if !isChecked.contains(String(indexPath.row)){
+            isChecked.append(String(indexPath.row))
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else{
+            if let i = isChecked.index(of: String(indexPath.row)){
+                isChecked.remove(at: i)
+            }
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        
+        //tableView.reloadData()
     }
 
 
